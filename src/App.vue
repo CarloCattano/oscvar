51ae08dd5c0d4b30625688aa6a4dfe948f55fc13
CarloCattano <freevibes@gmail.com>
2020-10-30 10:13:17 +0100

    indent

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <NodePanel msg="I/O"/>
-  </div>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <NodePanel msg="I/O"/>
+</div>
 </template>
-
 <script>
 import NodePanel from './components/NodePanel.vue'
-
 export default {
-  name: 'App',
-  components: {
-    NodePanel
-  }
+name: 'App',
+components: {
+  NodePanel
+}
 }
 </script>
 
--- src/components/IO_node.vue
+++ src/components/IO_node.vue
@@ -1,19 +1,19 @@
 <template>
-	<div id="io_node">
-		<p>{{ mindex }}</p>
-		<div class="standbyme">
-			<vue-dropdown
-				:config="config_mode" @setSelectedOption="setNewSelectedOption($event,config_mode);"></vue-dropdown>
-		</div>
-		<div class="standbyme">
-			<vue-dropdown :config="config_range" @setSelectedOption="setNewSelectedOption($event,config_range);">
-			</vue-dropdown>
-		</div>
-			<div class="standbyme">
-			<vue-dropdown :config="config_name" @setSelectedOption="setNewSelectedOption($event,config_range);">
-			</vue-dropdown>
-		</div>
-	</div>
+<div id="io_node">
+  <p>{{ mindex }}</p>
+  <div class="standbyme">
+    <vue-dropdown
+      :config="config_mode" @setSelectedOption="setNewSelectedOption($event,config_mode);"></vue-dropdown>
+  </div>
+  <div class="standbyme">
+    <vue-dropdown :config="config_range" @setSelectedOption="setNewSelectedOption($event,config_range);">
+    </vue-dropdown>
+  </div>
+    <div class="standbyme">
+    <vue-dropdown :config="config_name" @setSelectedOption="setNewSelectedOption($event,config_range);">
+    </vue-dropdown>
+  </div>
+</div>
 </template>
 <script>
 
@@ -21,111 +21,111 @@ import vueDropdown from "../../node_modules/vue-dynamic-dropdown/dropdown";
 import cv_config from "../cv_config.json"
 
 export default {
-	name: "io_node",
-	components: {
-		vueDropdown
-	},
-	props:['mindex'],
-	data: function() {
-		return {
-			cvConfigData : cv_config,
-			config_mode: {
-				options: [
-					{
-						value: "CV out"
-					},
-					{
-						value: "CV In"
-					},
-					{
-						value: "Digital Im"
-					}
-				],
-				
-				placeholder: "Mode",
-				backgroundColor: "#cde4f5",
-				textColor: "black",
-				borderRadius: "1.5em",
-				border: "1px solid gray",
-				width: 180
-			},
-			config_range: {
-				options: [
-					{
-						value: "-5 5V"
-					},
-					{
-						value: "0 10V"
-					},
-					{
-						value: "-2 5V"
-					}
-				],
-				
-				placeholder: "Range",
-				backgroundColor: "#cde4f5",
-				textColor: "black",
-				borderRadius: "1.5em",
-				border: "1px solid gray",
-				width: 180
-			},	
-			config_name: {
-				options: ["Add yours"],
-				placeholder: cv_config['cv'][this.mindex-1]['name'],
-				backgroundColor: "#cde4f5",
-				textColor: "black",
-				borderRadius: "1.5em",
-				border: "1px solid gray",
-				width: 300
-			}
-		};
-	},
-	created:function() {
+name: "io_node",
+components: {
+  vueDropdown
+},
+props:['mindex'],
+data: function() {
+  return {
+    cvConfigData : cv_config,
+    config_mode: {
+      options: [
+        {
+          value: "CV out"
+        },
+        {
+          value: "CV In"
+        },
+        {
+          value: "Digital Im"
+        }
+      ],
+      
+      placeholder: "Mode",
+      backgroundColor: "#cde4f5",
+      textColor: "black",
+      borderRadius: "1.5em",
+      border: "1px solid gray",
+      width: 180
+    },
+    config_range: {
+      options: [
+        {
+          value: "-5 5V"
+        },
+        {
+          value: "0 10V"
+        },
+        {
+          value: "-2 5V"
+        }
+      ],
+      
+      placeholder: "Range",
+      backgroundColor: "#cde4f5",
+      textColor: "black",
+      borderRadius: "1.5em",
+      border: "1px solid gray",
+      width: 180
+    },	
+    config_name: {
+      options: ["Add yours"],
+      placeholder: cv_config['cv'][this.mindex-1]['name'],
+      backgroundColor: "#cde4f5",
+      textColor: "black",
+      borderRadius: "1.5em",
+      border: "1px solid gray",
+      width: 300
+    }
+  };
+},
+created:function() {
 
-		var initMode = (this.cvConfigData['cv'][this.mindex-1]['mode'])   
-		var initRange =(this.cvConfigData['cv'][this.mindex-1]['range'])     
-  
-		console.log(initMode + " MODE")
+  var initMode = (this.cvConfigData['cv'][this.mindex-1]['mode'])   
+  var initRange =(this.cvConfigData['cv'][this.mindex-1]['range'])     
 
-		this.config_mode.placeholder = this.config_mode.options[initMode].value
-		this.config_range.placeholder = this.config_range.options[initRange].value
-	},
+  console.log(initMode + " MODE")
 
-	methods: {
-		setNewSelectedOption(selectedOption,property) {
-			property.placeholder = selectedOption.value
-			// TO DO send to JSON property with IO index and props 
-			if(property == this.config_range){
-				console.log("INDEX "+ this.mindex + " Range : "+ selectedOption.value)
-			}
-			if(property == this.config_mode){
-				console.log("INDEX "+ this.mindex + "Mode : "+ selectedOption.value)
-			}
-	
-		}
-	},
+  this.config_mode.placeholder = this.config_mode.options[initMode].value
+  this.config_range.placeholder = this.config_range.options[initRange].value
+},
+
+methods: {
+  setNewSelectedOption(selectedOption,property) {
+    property.placeholder = selectedOption.value
+    // TO DO send to JSON property with IO index and props 
+    if(property == this.config_range){
+      console.log("INDEX "+ this.mindex + " Range : "+ selectedOption.value)
+    }
+    if(property == this.config_mode){
+      console.log("INDEX "+ this.mindex + "Mode : "+ selectedOption.value)
+    }
+
+  }
+},
 };
 
 </script>
 <style>
 #io_node {
 
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: left;
-	color: #2c3e50;
-	margin-top: 0.5rem;
+font-family: "Avenir", Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: left;
+color: #2c3e50;
+margin-top: 0.5rem;
 }
 
 .standbyme {
-	text-align: left;
-	display: inline-block;
+text-align: left;
+display: inline-block;
 }
 p{
-	margin: 0;
-	padding: 0%;
-	display: inline;
-	margin-right: 0.4rem;
+margin: 0;
+padding: 0%;
+display: inline;
+margin-right: 0.4rem;
 }
 </style>
